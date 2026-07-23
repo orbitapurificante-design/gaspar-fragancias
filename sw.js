fdd4d10cab21ced169437202d5191922f48932d8
orbitapurificante-design <orbitapurificante@gmail.com>
2026-07-24 00:38:31 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'gaspar-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache){ return cache.addAll(ASSETS); }).catch(function(){})
+  );
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(function(){ return caches.match(e.request); })
+  );
+});
